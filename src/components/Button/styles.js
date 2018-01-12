219c7d5866c8643a400af118e8028e760b777668
forlenzamaximiliano <forlenzamaximiliano@gmail.com>
2018-01-12 10:45:20 -0300

    feat(src): add Button component and fix title in README

---
+++ src/components/Button/styles.js
@@ -0,0 +1,41 @@
+export default {
+    button: {
+        button: {
+            borderRadius: 4,
+            minWidth: 250
+        },
+        buttonDefault: {
+            backgroundColor: '#DCDCDC',
+            borderWidth: 1,
+            borderColor: '#7D7D7D'
+        },
+        buttonPrimary: {
+            backgroundColor: '#0E516E'
+        },
+        buttonDanger: {
+            backgroundColor: 'white',
+            borderWidth: 1,
+            borderColor: 'red'
+        }
+    },
+    buttonText: {
+        button: {},
+        buttonDefault: {
+            color: '#333333'
+        },
+        buttonPrimary: {},
+        buttonDanger: {
+            color: 'red'
+        }
+    },
+    buttonIcon: {
+        button: {},
+        buttonDefault: {
+            color: '#333333'
+        },
+        buttonPrimary: {},
+        buttonDanger: {
+            color: 'red'
+        }
+    }
+};
