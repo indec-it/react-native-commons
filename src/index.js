5d214b7b5a1f66be97e67a49db47f37ad5ad13e6
maximilianoforlenza <forlenzamaximiliano@gmail.com>
2018-01-12 08:43:10 -0300

    feat(src): add Col component (#3)

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
+import Col from './components/Col';
 import LoadingIndicator from './components/LoadingIndicator';
 import Row from './components/Row';
 import SectionTitle from './components/SectionTitle';
 import TextError from './components/TextError';
 import TextStrong from './components/TextStrong';
 
+export {Col};
 export {LoadingIndicator};
 export {Row};
 export {SectionTitle};
