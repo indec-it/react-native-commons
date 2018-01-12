2aee49531352b58c4704f4157a5147c01ccbed44
maximilianoforlenza <forlenzamaximiliano@gmail.com>
2018-01-12 10:30:28 -0300

    feat(src): add Header component (#5)

---
+++ src/components/Header/styles.js
@@ -0,0 +1,37 @@
+export default {
+    brandContainer: {
+        height: 96,
+        paddingRight: 10,
+        paddingLeft: 10,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        backgroundColor: '#056a8e'
+    },
+    brandImage: {
+        width: 200,
+        height: 76
+    },
+    text: {
+        fontWeight: 'bold',
+        fontSize: 38,
+        color: '#fff'
+    },
+    navContainer: {
+        height: 45,
+        paddingRight: 10,
+        paddingLeft: 10,
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        backgroundColor: '#0da887'
+    },
+    navText: {
+        fontSize: 18,
+        color: '#fff'
+    },
+    navIcon: {
+        color: '#fff',
+        marginRight: 10
+    }
+};
--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import Col from './components/Col';
+import Header from './components/Header';
 import LoadingIndicator from './components/LoadingIndicator';
 import Row from './components/Row';
 import SectionTitle from './components/SectionTitle';
@@ -9,6 +10,7 @@ export const getFontAwesome = (name, color, size) => ({
     name, type: 'font-awesome', color, size
 });
 export {Col};
+export {Header};
 export {LoadingIndicator};
 export {Row};
 export {SectionTitle};
