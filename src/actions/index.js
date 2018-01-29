11884ae21fd4a49abe0624516e34f79954cd7988
Maximiliano Forlenza <forlenzamaximiliano@gmail.com>
2018-01-29 15:31:25 -0300

    fix(actions): export handleError action

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,6 @@
-import {ERROR_OCCURRED} from './common';
+import {ERROR_OCCURRED, handleError} from './common';
 import {USER_LOGIN_REQUESTED, USER_TOKEN_REQUESTED} from './session';
 
 export {ERROR_OCCURRED};
+export {handleError};
 export {USER_LOGIN_REQUESTED, USER_TOKEN_REQUESTED};
