810406fab19f2cc2ee58f02373c293abfba8baae
pbasti <p.bastidascarbonell@gmail.com>
2018-02-28 13:29:43 -0300

    style(tabNavigator): style tab and selectedTab (#36)

--- src/components/TabNavigator/styles.js
+++ src/components/TabNavigator/styles.js
@@ -3,22 +3,26 @@ import {StyleSheet} from 'react-native';
 export default StyleSheet.create({
     container: {
         flexDirection: 'row',
-        marginTop: 8
+        marginTop: 12,
+        marginLeft: 20
     },
     tab: {
-        flex: 1,
+        flex: 0.2,
         backgroundColor: '#e4e4e4',
         padding: 16,
         marginRight: 8,
-        marginLeft: 8,
+        marginLeft: -6,
         borderColor: '#cecece',
         borderWidth: 0.5,
-        marginTop: 8
+        marginTop: 8,
+        borderTopRightRadius: 8,
+        borderTopLeftRadius: 8,
+        fontSize: 18
     },
     selectedTab: {
         backgroundColor: '#008bc7',
         color: 'white',
-        marginTop: 0
+        marginTop: 8
     },
     tabNavigatorFooter: {
         backgroundColor: '#cecece',
