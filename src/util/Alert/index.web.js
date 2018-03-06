becac22c9691c9143a829acdbd7193122cad948b
Juancoot <juan_manuel_lopez_1996@hotmail.com>
2018-03-06 11:53:46 -0300

    feat(util): add Alert util (#40)

---
+++ src/util/Alert/index.web.js
@@ -0,0 +1,31 @@
+/* global window */
+import {isEmpty, isFunction, head, last} from 'lodash';
+
+const triggerPressHandler = button => {
+    const {onPress} = button;
+    if (isFunction(onPress)) {
+        onPress();
+    }
+};
+
+class Alert {
+    static alert(title, text, buttons) {
+        if (buttons.length > 2) {
+            throw new Error('Alert-web: dismiss button is not supported.');
+        }
+        if (buttons.length === 2) {
+            if (window.confirm(`${title} - ${text}`)) {
+                triggerPressHandler(last(buttons));
+            } else {
+                triggerPressHandler(head(buttons));
+            }
+            return;
+        }
+        window.alert(`${title} - ${text}`);
+        if (!isEmpty(buttons)) {
+            triggerPressHandler(head(buttons));
+        }
+    }
+}
+
+export default Alert;
--- src/util/index.js
+++ src/util/index.js
@@ -1,9 +1,11 @@
 import childrenPropType from './childrenPropType';
 import getFontAwesome from './getFontAwesome';
+import Alert from './Alert';
 import mergeStyles from './mergeStyles';
 import stylePropType from './stylePropType';
 
 export {childrenPropType};
 export {getFontAwesome};
+export {Alert};
 export {mergeStyles};
 export {stylePropType};
