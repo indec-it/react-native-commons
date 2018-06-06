f976e097b74670e9561b3e3d3b1d9e231c0a7248
Maximiliano Forlenza <forlenzamaximiliano@gmail.com>
2018-06-06 12:00:47 -0300

    feat(services): add dateUtilsService

--- src/services/index.js
+++ src/services/index.js
@@ -1,5 +1,7 @@
+import DateUtilsService from './dateUtils';
 import SessionService from './session';
 import StorageService from './storage';
 
+export {DateUtilsService};
 export {SessionService};
 export {StorageService};
