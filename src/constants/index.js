97574882f89593e7a7c5fef9c45edb8445dd7b9d
juanlopez1 <juan_manuel_lopez_1996@hotmail.com>
2019-02-20 16:35:52 -0300

    style(modalConfirm): re work styles

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,4 @@
-const CONFIRM_LOGOUT_MESSAGE = 'Desea salir de la aplicacion?';
+const CONFIRM_LOGOUT_MESSAGE = '¿Desea salir de la aplicacion?';
 
 // eslint-disable-next-line
 export {CONFIRM_LOGOUT_MESSAGE};
